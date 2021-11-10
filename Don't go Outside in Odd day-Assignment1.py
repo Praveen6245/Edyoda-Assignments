b2350a4e05364e9ccacdbde0d9a33f377d4a4aa5
Praveen6245 <94069246+Praveen6245@users.noreply.github.com>
2021-11-10 22:47:08 +0530

    Add files via upload

---
+++ Don't go Outside in Odd day-Assignment1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Don't go Outside in Odd day
+
+# In[7]:
+
+
+c=[]
+while(1):
+    i=input()
+    if(i!=""):#Ends when enter is hit twice
+        c.append(i)
+    else:
+        break
+count=0
+for i in c:
+    if(int(i) & 1):
+        count+=1
+print(count,len(c)-count)
+
