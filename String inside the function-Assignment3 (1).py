dc69780ed361bfaf45046abb92ee30214603f9a4
Praveen6245 <94069246+Praveen6245@users.noreply.github.com>
2021-11-21 21:29:47 +0530

    Add files via upload

---
+++ String inside the function-Assignment3 (1).py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+input_string=input()# Take the input string
+print(input_string[::-1]) # Traverse through the string in reverse order using slicing operator
+
