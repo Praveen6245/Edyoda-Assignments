040ee2bb6253a4f1011494f137fa9ed0398009ab
Praveen6245 <94069246+Praveen6245@users.noreply.github.com>
2021-12-05 21:57:51 +0530

    Add files via upload

---
+++ Play with Lambda - Assignment 3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x=int(input()) # Taking input integer type
+add_using_lambda=lambda x: x+25 # Passing x into the lambda function
+print(add_using_lambda(x)) # Lambda function call
+
